2d5c538618db06a4e70b512fc42f67bf4dff57cf
Lorenzo <lorenzozar@gmail.com>
2021-05-21 09:55:58 +0300

    http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularism';
+  subscription: Subscription;
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    this.subscription = this.appService
+      .getData()
+      .subscribe((res) => console.log(res));
+
+    console.log(this.subscription);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    // console.log(this.subscription); provare negli altri componenti
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
