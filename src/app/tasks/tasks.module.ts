a618069e9f27dad9ff68b2b8298df31e23fc4a7a
Lorenzo <lorenzozar@gmail.com>
2021-06-14 10:30:27 +0200

    custom pipe to log form prop

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { TasksRoutingModule } from './tasks-routing.module';
 import { TasksComponent } from './tasks.component';
+import { FormsModule } from '@angular/forms'; // to use ngForm
+import { CommonModule } from '@angular/common'; // necessary to pipe json
+
+import { LogPipe } from './log.pipe';
 
 @NgModule({
-  declarations: [TasksComponent],
-  imports: [TasksRoutingModule],
+  declarations: [TasksComponent, LogPipe],
+  imports: [TasksRoutingModule, FormsModule, CommonModule],
 })
 export class TasksModule {}
