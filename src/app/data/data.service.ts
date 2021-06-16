b0df9db51b827f439e4716666a8957aef8deae6d
Lorenzo <lorenzozar@gmail.com>
2021-06-16 14:25:42 +0200

    http post

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,16 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { DefaultData } from './default-data';
+
+// dataService manages all HTTP access in the app
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  constructor() {}
+  // inject http client
+  constructor(private http: HttpClient) {}
 
-  postTasksForm(defaultData: DefaultData): Observable<DefaultData> {
+  postTasksForm(defaultData: DefaultData): Observable<any> {
     // of function of rsjx creates a new observable
     // passing back defaultData
-    return of(defaultData);
+    // return of(defaultData);
+
+    // using http variable to post the form
+    // HttpClient works with Observables so we can return the post request
+    // See https://putsreq.com/gN8gga9FTCm8M7d5y9CI/inspect
+    return this.http.post(
+      'https://putsreq.com/gN8gga9FTCm8M7d5y9CI',
+      defaultData
+    );
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { DataService } from '../data/data.service';
@@ -28,11 +29,23 @@ export class TasksComponent implements OnInit {
   onSubmit(form: NgForm) {
     console.log('In onSubmit: ', form.valid);
 
-    // returns an observable
-    // then subscribe to it
-    this.dataService.postTasksForm(this.copiedDefaultData).subscribe(
-      (result) => console.log('success: ', result),
-      (error) => console.log('error', error)
-    );
+    if (form.valid) {
+      // returns an observable
+      // then subscribe to it
+      this.dataService.postTasksForm(this.copiedDefaultData).subscribe(
+        (result) => console.log('success: ', result),
+        (error) => this.onHttpError(error)
+      );
+    } else {
+      this.postError = true;
+      this.postErrorMessage = 'Please, fix the error';
+    }
+  }
+  postError = false;
+  postErrorMessage = '';
+  onHttpError(errorResponse: any): void {
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
   }
 }
